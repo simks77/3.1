35ffa8735c39faacb5d717d6c0543b86af554169
simks77 <sim_ks77@yahoo.com>
2025-08-13 22:26:55 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,13 +4,9 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "kssim-bucket"
+    bucket = "sctp-ce10-tfstate"
     key    = "terraform.tfstate"
     region = "ap-southeast-1"
   }
 }
 
-# Add any resources you want (e.g., S3 bucket)
-resource "aws_s3_bucket" "example" {
-  bucket = "kssim-bucket"
-}
